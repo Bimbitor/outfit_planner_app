82cd232d065ec593d608384fc2b5771c8f18acc0
SebastianCarron <ycarron@ucompensar.edu.co>
2024-10-12 17:51:20 -0500

    correcion de icono, y vistas sebas 12 oct 2024

--- app/src/main/java/com/example/outfit_planner_app/MainActivity.java
+++ app/src/main/java/com/example/outfit_planner_app/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.calendario_usuario_viewgit push);
+        setContentView(R.layout.crear_outfit_usuario_view);
 
     }
 }
