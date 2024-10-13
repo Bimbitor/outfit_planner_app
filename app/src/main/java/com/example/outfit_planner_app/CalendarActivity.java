1099267610aac415ba6fcc80b42334a365288c5d
Bimbitor <javijohanro30@gmail.com>
2024-10-12 23:03:07 -0500

    se agrega conexión entre Activities y Botones

--- app/src/main/java/com/example/outfit_planner_app/CalendarActivity.java
+++ app/src/main/java/com/example/outfit_planner_app/CalendarActivity.java
@@ -1,8 +1,5 @@
 package com.example.outfit_planner_app;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +10,6 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.calendar_activity);
+        setContentView(R.layout.activity_calendar);
     }
 }
--- app/src/main/java/com/example/outfit_planner_app/OutfitsListActivity.java
+++ app/src/main/java/com/example/outfit_planner_app/OutfitsListActivity.java
@@ -2,7 +2,7 @@ package com.example.outfit_planner_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +15,7 @@ public class OutfitsListActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_outfit_list);
 
-        Button addButton = findViewById(R.id.button_outfits_list_add);
+        ImageButton addButton = findViewById(R.id.button_outfits_list_add);
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
